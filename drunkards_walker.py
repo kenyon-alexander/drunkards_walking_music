acb849ddfb70563d90b291205e5b833a2713f1b5
Kenyon Alexander <kenyon.alexander@alloy.ai>
2022-02-21 22:34:55 -0800

    Build most basic set iterator based on random walk

---
+++ drunkards_walker.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+import numpy as np
+import random
+import drunkards_helper
+
+ITERATIONS = 1000
+
+def create_drunkards_walking_music(pitch_sets, distance_matrix):
+	current_set = random.randint(0,len(pitch_sets)-1)
+	drunkards_walking_music = [pitch_sets[current_set]]
+	for i in range(ITERATIONS):
+		r = random.uniform(0,1)
+		j = 0
+		while distance_matrix[current_set][j] < r:
+			j +=1
+		drunkards_walking_music.append(pitch_sets[j])
+		current_set = j
+	return(drunkards_walking_music)
+
+if __name__ == "__main__":
+	pitch_sets = drunkards_helper.create_pitch_sets(
+		drunkards_helper.FULL_PITCH_SETS
+	)
+	distance_matrix = drunkards_helper.create_distance_matrix(pitch_sets)
+	drunkards_walking_music = create_drunkards_walking_music(
+		pitch_sets, 
+		distance_matrix
+	)
+	print(drunkards_walking_music)
